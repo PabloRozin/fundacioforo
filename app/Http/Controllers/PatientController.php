19bbfd5e56a0156fd7b300a4a953717c6e13f5af
Pablo Rozin <pablorozin91@gmail.com>
2020-04-01 14:50:44 -0300

    changes

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -1568,6 +1568,8 @@ class PatientController extends AdminController
             $data['hcDates']['patients'][$hcDate->patient_id]['consultationTypes'][$hcDate->type]['count'] = (! isset($data['hcDates']['patients'][$hcDate->patient_id]['consultationTypes'][$hcDate->type]['count'])) ? 1 : $data['hcDates']['patients'][$hcDate->patient_id]['consultationTypes'][$hcDate->type]['count'] + 1;
         }
 
+        dd($data['hcDates']);
+
         $admissions = $this->account->patientAdmissions()
             ->orderBy('patient_id', 'ASC')
             ->orderBy('professional_id', 'ASC')
